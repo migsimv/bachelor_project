b34e32f28f9220a49c012b2b2d6429a68d271e13
Migle Simaviciute <simmigle8@gmail.com>
2023-08-14 16:24:41 +0300

    implemented core finding by array values and some fixes

--- helpers.py
+++ helpers.py
@@ -2,6 +2,8 @@ import os
 import matplotlib.pyplot as plt
 import base64  
 import io
+import random
+import math
 
 
 def get_files_in_uploads_folder():
@@ -45,7 +47,7 @@ def getResult(graph):
     result = []
     for key, value in graph.items():
         res = (key, "->", value)
-        line = "{} {} {}".format(res[0], res[1], ' '.join(str(x) for x in res[2]))
+        line = "{} {} {}".format(res[0], res[1], ', '.join(str(x) for x in res[2]))
         result.append(line)
     return result
 
@@ -100,3 +102,45 @@ def generate_plot(graph, name): #BUG nucrashina po kurio laiko RuntimeError: mai
 
     plt.close() 
     return plot_data
+
+def longest_inner_array_length(array_of_arrays):
+    longest_length = 0
+    for inner_array in array_of_arrays:
+        current_length = len(inner_array)
+        if current_length > longest_length:
+            longest_length = current_length
+    return longest_length
+
+def getGraph(decoded_lines):
+    graph = {}
+    for line in decoded_lines:
+        parts = line.split('->')
+        node = int(parts[0])
+        neighbors = list(map(int, parts[1].split()))
+        graph[node] = neighbors
+    return graph
+
+def create_bipartite_graph(xArray, yArray, alpha):
+    V = len(xArray) + len(yArray)
+    adj_list = [[] for i in range(V)]
+    for i in range(len(xArray)):
+        f = 0
+        for j in range(len(xArray), V):
+            p = getP(alpha, xArray[i], yArray[f], len(xArray), len(yArray))
+            if random.random() < p:
+                adj_list[i].append(j)
+                adj_list[j].append(i)
+            f += 1
+    return adj_list
+def getP(alpha,x,y,n,m):
+    return alpha * (x*y) / math.sqrt(n*m)
+
+def findConnectedActors(x, graph): 
+    neighbors = {}
+    new_graph = {}
+    for i in range(x):
+        for neighbor in graph[i]:
+            neighbors.setdefault(neighbor, []).append(i)
+    for i in range(x):
+        new_graph[i] = list({n for neighbor in graph[i] for n in neighbors.get(neighbor, {}) if n != i})
+    return new_graph
--- views.py
+++ views.py
@@ -1,63 +1,74 @@
-from flask import Blueprint, url_for,redirect,render_template, request, jsonify, make_response
+from flask import Blueprint, render_template, request
 import helpers
-from flask import Flask, request
-from werkzeug.utils import secure_filename
 
 views = Blueprint(__name__,'views')
 
 @views.route('/upload_file', methods=['POST'])
 def upload_file_route():
     file = request.files['fileUpl']
-    print("AAAA")
     if file:
         file.save('uploads/' + file.filename)
         return 'File uploaded successfully', 200
     else:
         return 'No file uploaded', 400
-    
-def get_result_file(file, k):
-    print(file, k)
-    return 0
+
+def get_result_from_adj_list(graph, k):
+    data = {}
+    graphComponents = helpers.find_components(graph)
+    data["graphComponents"] =len( graphComponents)
+    data["graphVertices"] = len(graph)
+    data["maxGraphComponent"] = helpers.longest_inner_array_length(graphComponents)
+    data["core"] = helpers.getCore(graph, k)
+    data["coreToPrint"] = helpers.getResult(data["core"])
+    data["coreVertices"] = len(data["core"])
+    coreComponents = helpers.find_components(data["core"])
+    data["coreComponents"] = len(coreComponents)
+    data["maxCoreComponent"] = helpers.longest_inner_array_length(coreComponents)
+
+    return data
+
 
 @views.route('/', methods=['GET', 'POST'])
 def index():
     files = helpers.get_files_in_uploads_folder()
     if request.method == 'POST':
-            if request.form.get('selected_file'):
-                decoded_lines = helpers.read_selected_file_content(request.form.get('selected_file'))
+            if request.form['options'] == 'option1': #getting graph
+                if request.form.get('selected_file'):
+                    decoded_lines = helpers.read_selected_file_content(request.form.get('selected_file'))
+                else:
+                    decoded_lines = []
+                    file = request.files['fileUpl']
+                    lines = file.readlines()  
+                    for line in lines:
+                        decoded_line = line.decode('utf-8')  
+                        decoded_lines.append(decoded_line.strip()) 
+                graph = helpers.getGraph(decoded_lines)
+            elif request.form['options'] == 'option2':
+                xLen =  int(request.form.get('xLen'))
+                xWeight =  int(request.form.get('xWeight'))
+                yLen =  int(request.form.get('yLen'))
+                yWeight =  int(request.form.get('yWeight'))
+                alpha = float(request.form.get('alpha'))
+                xArray = [xWeight for _ in range(xLen)]
+                yArray = [yWeight for _ in range(yLen)]
+                bipartiteGraph = helpers.create_bipartite_graph(xArray, yArray, alpha)
+                graph = helpers.findConnectedActors(xLen, bipartiteGraph) #actorsGraph
             else:
-                decoded_lines = []
-                file = request.files['fileUpl']
-                lines = file.readlines()  
-                for line in lines:
-                    decoded_line = line.decode('utf-8')  
-                    decoded_lines.append(decoded_line.strip()) 
-            b = request.form['digit']
-            # print(file)
-            # get_result_file(a,b)
-            graph = {}
-            for line in decoded_lines:
-              parts = line.split('->')
-              node = int(parts[0])
-              neighbors = list(map(int, parts[1].split()))
-              graph[node] = neighbors
-            print(graph)
-            # core = helpers.getCore(graph, b)
-            # res = helpers.getResult(core)
-            # komp = helpers.find_components(core)
-            # ilg = komp[0]
-
-            # for array in komp:
-            #     ilg = [len(array) for array in komp]
-
-            # komp = helpers.find_components(core)
-            # virsunes = len(core)
+                xArray = [int(value.strip()) for value in (request.form.get('xArray')).split(",")]
+                yArray = [int(value.strip()) for value in (request.form.get('yArray')).split(",")]
+                alpha = float(request.form.get('alpha2'))
+                bipartiteGraph = helpers.create_bipartite_graph(xArray, yArray, alpha)
+                graph = helpers.findConnectedActors(len(xArray), bipartiteGraph) #actorsGraph
+            #working with the graph
+            k = request.form['digit']
+            data = get_result_from_adj_list(graph, k)
+            data['k'] = k
+            data['originalGraph'] =  helpers.getResult(graph)
             # plot_data_before = generate_plot(core, 'test5')
             # plot_data_core = generate_plot(graph, 'test')
             plot_data_before = 0
             plot_data_core =0
-            return render_template('res.html',virsunes=[])
-
-            # return render_template('res.html',virsunes=virsunes,plot_data_before = plot_data_core, plot_data = plot_data_before, selected_content =a, selected_content2 =res, modified_content=b, komponentes = len(komp), ilgiausia = len(ilg))
+            # print(data['originalGraph'])
+            # print(data['coreToPrint'])
+            return render_template('res.html', files=files, data=data)
     return render_template('index.html', files = files)
-
